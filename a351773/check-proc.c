06081e06b667d754a46b2191d7d2f0f164ece0db
Robin López V <140976722+Robin-LoVer@users.noreply.github.com>
2024-05-13 13:24:31 -0600

    Added explanatory comments to check-proc.c

--- a351773/check-proc.c
+++ a351773/check-proc.c
@@ -6,8 +6,8 @@
 
 char fetch_process(char *filename, char *procname);
 
-
 int main(int argc, char *argv[]) {
+  //Inicialización de variables
   struct dirent *entry;
   DIR *pDir;
   char proc_dir[] = "/proc";
@@ -21,12 +21,13 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
+  //Revisa cada directorio en /proc y sólo continua el programa si el directorio es numérico
+  //Directorios que no son numéricos tampoco son procesos
   while((entry = readdir(pDir)) != NULL) {
     if (!isdigit(entry->d_name[0]))
       continue;
 
-      FILE *cmdfile;
-      char auxname[128];
+      //Obtiene el nombre del directorio y lo imprime junto a su ID
       sprintf(proc_id_dir, "/proc/%s/cmdline", entry->d_name);
       isKernel = fetch_process(proc_id_dir, cmdline);
       printf("[%c] %s:%s\n", isKernel, entry->d_name, cmdline);
@@ -39,19 +40,24 @@ int main(int argc, char *argv[]) {
 
 
 char fetch_process(char *filename, char *procname) {
+  //Función para obtener el nombre del proceso
   FILE *file_pointer;
   char *line = NULL;
   char *cmp;
   size_t len = 0;
   procname[0] = '\0';
 
+  //Verifica que el nombre proveído no sea nulo y abre el archivo 'cmdline' del proceso
   if(filename)
-  file_pointer = fopen(filename,"r");
+    file_pointer = fopen(filename,"r");
+
+  //Verifica que el archivo 'cmdline' del proceso exista
   if(file_pointer == NULL) {
     printf("Error while opening file '%s'\nExit code 2\n", filename);
     exit(2);
   }
 
+  //Lee el contendio del archivo línea por línea
   while(getline(&line, &len, file_pointer) != -1) {
     sprintf(procname, "%s%s", procname, line);
     cmp = line;
@@ -59,6 +65,9 @@ char fetch_process(char *filename, char *procname) {
 
   fclose(file_pointer);
   free(line);
+
+  //La variable cmp guarda la última línea del archivo
+  //Si está vacía regresa 'K' para proceso de kernel, sino 'U' de sistema de usuario
   if(!cmp)
     return 'K';
   else
