1665b27fe815a8d30d7fa849918f9afe1b921e96
Adrian Gonzalez <adrigondo.profesional@gmail.com>
2024-05-15 19:42:14 -0600

    Getting and printing process name

--- Equipo-DALI/Proyecto_DALI.c
+++ Equipo-DALI/Proyecto_DALI.c
@@ -4,15 +4,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-void get_process_cmd(char *filename, char *procname);
+void get_process_cmdline(char *filename, char *procname);
+void get_process_name(char *pid, char *procname);
 
 int main(int argc, char *argv[])
 {
         struct dirent *entry;
         DIR *pDir;
         char proc_dir[] = "/proc";
-        char proc_id_dir[1000];
-        char cmdline[255];
+        char proc_id_cmdline_dir[255];
+        char proc_id_status_dir[255];
+        char proc_cmdline[10000];
+        char proc_name[255];
+        char kernel_string[10] = "";
         FILE *fp;
 
         pDir = opendir(proc_dir);
@@ -27,23 +31,26 @@ int main(int argc, char *argv[])
                 if (!isdigit(entry->d_name[0]))
                         continue;
 
-                sprintf(proc_id_dir, "/proc/%s/cmdline", entry->d_name);
+                strcpy(proc_name, "");
+                strcpy(kernel_string, "");
 
-                get_process_cmd(proc_id_dir, cmdline);
+                sprintf(proc_id_cmdline_dir, "/proc/%s/cmdline", entry->d_name);
+                sprintf(proc_id_status_dir, "/proc/%s/status", entry->d_name);
+                get_process_cmdline(proc_id_cmdline_dir, proc_cmdline);
+                get_process_name(proc_id_status_dir, proc_name);
 
-                char *isKernel = "";
-                if (strlen(cmdline) == 0)
+                if (strlen(proc_cmdline) == 0)
                 {
-                        isKernel = "(Kernel)";
+                        strcpy(kernel_string, "(Kernel)");
                 }
-                printf("| PID: %s %s|\n%s\n\n", entry->d_name, isKernel, cmdline);
+                printf("| PID: %s %s| NAME: %s |\n%s\n\n", entry->d_name, kernel_string, proc_name, proc_cmdline);
         }
 
         closedir(pDir);
         return 0;
 }
 
-void get_process_cmd(char *filename, char *procname)
+void get_process_cmdline(char *filename, char *procname)
 {
         FILE *fp;
         char *line = NULL;
@@ -53,6 +60,7 @@ void get_process_cmd(char *filename, char *procname)
         // Open file in read-only mode
         fp = fopen(filename, "r");
 
+        // If file opened successfully, then print the contents
         if (fp == NULL)
         {
                 printf("Error abriendo archivo %s\n", filename);
@@ -65,5 +73,32 @@ void get_process_cmd(char *filename, char *procname)
         }
 
         fclose(fp);
+        free(line);
+}
+
+void get_process_name(char *filename, char *procname)
+{
+        FILE *status_file;
+        char *line = NULL;
+        size_t len = 0;
+        procname[0] = '\0';
+
+        // Open file in read-only mode
+        status_file = fopen(filename, "r");
+
+        // If file opened successfully, then print the contents
+        if (status_file == NULL)
+        {
+                printf("Error abriendo archivo %s\n", filename);
+                exit(2);
+        }
+
+        if (getline(&line, &len, status_file) != -1)
+        {
+                line[strlen(line) - 1] = '\0';
+                sprintf(procname, "%s", line + 6);
+        }
+
+        fclose(status_file);
         free(line);
 }
